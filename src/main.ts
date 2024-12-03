5bc04f388492f88fef7bfa0bce31c65db33cda9e
baiel1234 <avtandilovbajel47@gmail.com>
2024-12-03 19:23:00 +0600

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Настройка Swagger
+  const config = new DocumentBuilder()
+    .setTitle('HeadHunter API')
+    .setDescription('API для управления вакансиями и их подробной информацией')
+    .setVersion('1.0')
+    .addBearerAuth() // Если вы используете JWT, добавьте эту строку
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/vacancies-info/vacancies-info.controller.ts
+++ src/vacancies-info/vacancies-info.controller.ts
@@ -2,35 +2,42 @@ import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/commo
 import { VacanciesInfoService } from './vacancies-info.service';
 import { UpdateVacancyInfoDto } from './dto/update-vacancy-info.dto';
 import { CreateVacancyInfoDto } from './dto/create-vacancy-info.dto';
+import { ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
 
+@ApiTags('Vacancy Info') // Название секции в Swagger
 @Controller('vacancies-info')
 export class VacanciesInfoController {
   constructor(private readonly vacanciesInfoService: VacanciesInfoService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Создать подробную информацию о вакансии' })
   create(@Body() createVacancyInfoDto: CreateVacancyInfoDto) {
     return this.vacanciesInfoService.create(createVacancyInfoDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Получить список всех описаний вакансий' })
   findAll() {
     return this.vacanciesInfoService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Получить подробности одной вакансии' })
+  @ApiParam({ name: 'id', description: 'ID описания вакансии' })
   findOne(@Param('id') id: string) {
     return this.vacanciesInfoService.findOne(+id);
   }
 
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateVacancyInfoDto: UpdateVacancyInfoDto,
-  ) {
+  @ApiOperation({ summary: 'Обновить описание вакансии' })
+  @ApiParam({ name: 'id', description: 'ID описания вакансии' })
+  update(@Param('id') id: string, @Body() updateVacancyInfoDto: UpdateVacancyInfoDto) {
     return this.vacanciesInfoService.update(+id, updateVacancyInfoDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Удалить описание вакансии' })
+  @ApiParam({ name: 'id', description: 'ID описания вакансии' })
   remove(@Param('id') id: string) {
     return this.vacanciesInfoService.remove(+id);
   }
