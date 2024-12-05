cdb8d5581fb2f9dfc24c00ee00d0a206803a46a4
baiel1234 <avtandilovbajel47@gmail.com>
2024-12-05 17:34:14 +0600

    vercel 3

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
 
-export async function createApp() { // Экспорт функции
+export async function createApp() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   const config = new DocumentBuilder()
     .setTitle('Headhunter API')
     .setDescription('API documentation for the Headhunter backend')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
   app.enableCors();
   await app.init();
+
   return server;
 }
-
