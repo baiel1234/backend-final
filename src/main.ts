27e1c1a90956cc10438e0996dcf641900723561e
baiel1234 <avtandilovbajel47@gmail.com>
2024-12-06 00:00:59 +0600

    vercel 17

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Handler, Context, Callback } from 'aws-lambda';
-import { Server } from 'http';
-import { createServer, proxy } from 'aws-serverless-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-let cachedServer: Server;
-
-async function createApp() {
-  const app = await NestFactory.create(AppModule);
+export async function createApp() {
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
@@ -16,21 +14,11 @@ async function createApp() {
     .setVersion('1.0')
     .addTag('API')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('/api/docs', app, document);
 
   app.enableCors();
   await app.init();
 
-  return createServer(app.getHttpAdapter().getInstance());
+  return server;
 }
-
-export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
-  if (!cachedServer) {
-    cachedServer = await createApp();
-  }
-  return proxy(cachedServer, event, context, 'PROMISE').promise;
-};
-
-
